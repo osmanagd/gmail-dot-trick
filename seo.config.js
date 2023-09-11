9d88bc60fe2f102547ca1c44f721751d7596d4ac
osmanagd <142901539+osmanagd@users.noreply.github.com>
2023-09-11 09:30:48 +0100

    Add files via upload

---
+++ seo.config.js
@@ -0,0 +1,19 @@
+// seo.config.js
+const defaultSEO = {
+  title: 'Gmail Dot Trick Generator',
+  description: 'Gmail dot trick generator,Google mail Trick-Gmail dot Trick-Fake gmail generator.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://Leiht.com',
+    site_name: 'Gmail dot trick generator',
+  },
+  twitter: {
+    handle: '@yourtwitterhandle',
+    site: '@yoursitetwitter',
+    cardType: 'summary_large_image',
+  },
+  keywords: ['Gmail dot trick generator',مولد اميلاات فرعية', 'Google mail Trick','mail dot Trick', 'gmail generator'], // Add your keywords here
+};
+
+export default defaultSEO;
